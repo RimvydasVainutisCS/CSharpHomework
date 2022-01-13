57f2aab423373a45ae63e149b31656df3b19f587
RimvydasVainutisCS <97608500+RimvydasVainutisCS@users.noreply.github.com>
2022-01-13 11:22:20 +0200

    202201131122

--- CSharpHomework/Program.cs
+++ CSharpHomework/Program.cs
@@ -16,7 +16,7 @@ namespace CSharpHomework
             var pointOne = new Point(3, 4);
             var pointTwo = new Point(4, 4);
             var pointThree = new Point(4, 3);
-            var pointFour = new Point(4, 4);
+            var pointFour = new Point(3, 4);
 
             //Console.WriteLine(pointOne.CoordinateX);
             //Console.WriteLine(pointOne.CoordinateY);
@@ -26,11 +26,15 @@ namespace CSharpHomework
             Console.WriteLine(pointThree.ToString());
             Console.WriteLine(pointFour.ToString());
 
-            Console.WriteLine("\n------");
+            Console.WriteLine("------");
+            Console.WriteLine();
 
-            Console.WriteLine(pointOne.Equals(pointTwo));
-            Console.WriteLine(pointOne.Equals(pointThree));
-            Console.WriteLine(pointOne.Equals(pointFour));
+            Console.WriteLine($"Are the coordinates {pointOne} equal to coordinates {pointTwo}? Answer: {pointOne.Equals(pointTwo)}.");
+            //Console.WriteLine(pointOne.Equals(pointTwo));
+            Console.WriteLine($"Are the coordinates {pointOne} equal to coordinates {pointThree}? Answer: {pointOne.Equals(pointThree)}.");
+            //Console.WriteLine(pointOne.Equals(pointThree));
+            Console.WriteLine($"Are the coordinates {pointOne} equal to coordinates {pointFour}? Answer: {pointOne.Equals(pointFour)}.");
+            //Console.WriteLine(pointOne.Equals(pointFour));
 
             Console.ReadLine();
         }
