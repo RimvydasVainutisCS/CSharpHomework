5317045e9d591e3cdfb3d5e3edb8e95bca8d255b
RimvydasVainutisCS <97608500+RimvydasVainutisCS@users.noreply.github.com>
2022-01-13 11:14:06 +0200

    Add project files.

---
+++ CSharpHomework/Point.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomework
+{
+    internal class Point
+    {
+        private int x;
+        private int y;
+
+        public int CoordinateX
+        {
+            get => x;
+            set => x = value;
+        }
+        public int CoordinateY
+        {
+            get => y;
+            set => y = value;
+        }
+
+        public Point(int x, int y)
+        {
+            CoordinateX = x;
+            CoordinateY = y;
+        }
+
+        public override string ToString()
+        {
+            return $"Coordinates: x = {CoordinateX}, y = {CoordinateY}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (!(obj is Point))
+            {
+                return false;
+            }
+
+            return (this.x == ((Point)obj).CoordinateX)
+                && (this.y == ((Point)obj).CoordinateY);
+        }
+    }
+}
---
+++ CSharpHomework/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Homework 001");
+            Console.WriteLine("\n------");
+
+            var pointOne = new Point(3, 4);
+            var pointTwo = new Point(4, 4);
+            var pointThree = new Point(4, 3);
+            var pointFour = new Point(4, 4);
+
+            //Console.WriteLine(pointOne.CoordinateX);
+            //Console.WriteLine(pointOne.CoordinateY);
+
+            Console.WriteLine(pointOne.ToString());
+            Console.WriteLine(pointTwo.ToString());
+            Console.WriteLine(pointThree.ToString());
+            Console.WriteLine(pointFour.ToString());
+
+            Console.WriteLine("\n------");
+
+            Console.WriteLine(pointOne.Equals(pointTwo));
+            Console.WriteLine(pointOne.Equals(pointThree));
+            Console.WriteLine(pointOne.Equals(pointFour));
+
+            Console.ReadLine();
+        }
+    }
+}
